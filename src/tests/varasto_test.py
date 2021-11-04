1c711497b4b329eab0b42933a2a0164c283e9d5c
Marja Tarvainen <marja.tarvainen@helsinki.fi>
2021-11-04 19:27:01 +0200

    Tehtävä 8, Unittest valmis.

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,66 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_ota_kaikki_oikein(self):
+        self.varasto.lisaa_varastoon(6)
+
+        saatu_maara = self.varasto.ota_varastosta(9)
+
+        # Pitäisi ottaa kaikki, mitä varastossa on.
+        self.assertAlmostEqual(saatu_maara, 6)
+
+    def test_ota_kaikki_varasto_tyhja(self):
+        self.varasto.lisaa_varastoon(6)
+
+        saatu_maara = self.varasto.ota_varastosta(9)
+
+        # Varaston pitäsi olla tyhjä, kun otetaan kaikki.
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def	test_ota_varastosta_negatiivinen(self):
+        self.varasto.lisaa_varastoon(8)
+        
+        saatu_maara = self.varasto.ota_varastosta(-10)
+   	
+        # Varaston saldon ei pitäisi muuttua
+        self.assertAlmostEqual(self.varasto.saldo, 8)
+
+    def test_lisays_lisaa_negatiivista_saldoa(self):
+        self.varasto.lisaa_varastoon(-10)
+
+        # Varastoon ei negatiivista saldoa
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_lisays_lisaa_liikaa(self):
+        self.varasto.lisaa_varastoon(8)
+        self.varasto.lisaa_varastoon(15)
+
+        # Varastoon mahtuu vain 10, loput katoaa
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_lisays_lisaa_sopivasti(self):
+        self.varasto.lisaa_varastoon(5)
+        self.varasto.lisaa_varastoon(2)
+
+        # Lisäys lasketaan oikein
+        self.assertAlmostEqual(self.varasto.saldo, 7)
+
+    def test_init_negatiivinen_varasto(self2):
+        self2.varasto = Varasto(-10)
+      
+        # Yritetään luoda Varastoa, joka negatiivinen.
+        self2.assertAlmostEqual(self2.varasto.tilavuus, 0)
+
+    def test_init_negatiivinen_varastouuuuuu(self3):
+        self3.varasto = Varasto(10, -8)
+
+        # Yritetään luoda varastoa, jonka alkusaldo negatiivinen
+        self3.assertAlmostEqual(self3.varasto.saldo, 0)
+
+    def test_varasto_merkkijonona_oikein(self):
+        self.varasto.lisaa_varastoon(5)
+        
+        # Varasto Stringinä tuoimii oikein
+        self.assertAlmostEqual(str(self.varasto), "saldo = 5, vielä tilaa 5")
+
