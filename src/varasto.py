0fca0c1d7ee768b96ee83ae8fc74d5df908d8bbe
Marja Tarvainen <marja.tarvainen@helsinki.fi>
2021-11-08 11:23:15 +0200

    Pylint, tehtävä 3 tehty.

--- src/varasto.py
+++ src/varasto.py
@@ -5,11 +5,10 @@ class Varasto:
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
-        
-        if alku_saldo < 0.0:
+        # voiko vain poistaa? if alku_saldo < 0.0:
             # virheellinen, nollataan
-            self.saldo = 0.0
-        elif alku_saldo <= tilavuus:
+        # voiko vain poistaa?    self.saldo = 0.0
+        if alku_saldo <= tilavuus: #elif vaihdettu if
             # mahtuu
             self.saldo = alku_saldo
         else:
@@ -31,15 +30,12 @@ class Varasto:
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
-
             return kaikki_mita_voidaan
-
         self.saldo = self.saldo - maara
-
         return maara
-    
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
